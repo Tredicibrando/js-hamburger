b013df6c7dca2629040ce4b7cb3497998e5deb30
Tredicibrando <nicolosigiov@gmail.com>
2022-02-08 16:09:56 +0100

    dichiaro const

---
+++ main.js
@@ -0,0 +1,4 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const hamburgerStripes = document.querySelector('.fas.fa-bars');
+const hamburgerClosed = document.querySelector('.close');
+
