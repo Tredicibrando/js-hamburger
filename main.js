7b9477d4a2ba15de9ff4c46ce6a866310534e984
Tredicibrando <nicolosigiov@gmail.com>
2022-02-08 16:11:46 +0100

    esercizio funzionante

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ const hamburgerMenu = document.querySelector('.hamburger-menu');
 const hamburgerStripes = document.querySelector('.fas.fa-bars');
 const hamburgerClosed = document.querySelector('.close');
 
+
+console.log(hamburgerMenu , hamburgerStripes)
+
+hamburgerStripes.addEventListener ("click", function() {
+    hamburgerMenu.style.display="block";
+})
+
+hamburgerClosed.addEventListener ("click", function() {
+    hamburgerMenu.style.visibility="hidden";
+})
